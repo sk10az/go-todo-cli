74ce8a2dcf669cc9402b11b6a5cc78a2c28531a0
Danil <sk10azik@gmail.com>
2023-02-25 13:50:47 +0300

    add todos

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"github.com/sk10az/go-todo-cli/"
+	"os"
+)
+
 const (
 	todoFile = ".todos.json"
 )
 
 func main() {
-	
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	del := flag.Int("del", 0, "delete a todo")
+	list := flag.Bool("list", false, "list all todos")
+
+	flag.Parse()
+
+	todos := &go_todo_cli.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Println(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+
+	}
 }
