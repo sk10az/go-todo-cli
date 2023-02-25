feafdd09a74e2207047a13824046e1e95bc01075
Danil <sk10azik@gmail.com>
2023-02-25 14:01:02 +0300

    completed main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
-	"github.com/sk10az/go-todo-cli/"
+	"github.com/sk10az/go-todo-cli"
+	"io"
 	"os"
+	"strings"
 )
 
 const (
@@ -28,6 +32,59 @@ func main() {
 
 	switch {
 	case *add:
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 
+		todos.Add(task)
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Println(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
 	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
